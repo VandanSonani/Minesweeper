7b883094d5d5f68f1c61cd4af12a1e10b490074e
VandanSonani <vsona2@uic.edu>
2024-12-19 15:35:35 -0600

    Init backend

---
+++ minesweeper-backend/src/main/java/com/example/minesweeperbackend/MinesweeperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.minesweeperbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinesweeperBackendApplication.class, args);
+    }
+
+}
