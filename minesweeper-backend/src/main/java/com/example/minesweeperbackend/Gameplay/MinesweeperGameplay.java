cf00330e6476f914e07f8ad3973b79ed3c55fb8f
VandanSonani <147519053+VandanSonani@users.noreply.github.com>
2024-12-23 16:51:28 -0600

    Delete minesweeper-backend/src/main/java/com/example/minesweeperbackend/Gameplay directory

--- minesweeper-backend/src/main/java/com/example/minesweeperbackend/Gameplay/MinesweeperGameplay.java
+++
@@ -1,8 +0,0 @@
-package com.example.minesweeperbackend.Gameplay;
-public class MinesweeperGameplay {
-// clicking placing flags and revealing cells
-    Player player;
-    Gameboard playerBoard = player.getGameboard();
-
-    // Handle clicking the board
-}
