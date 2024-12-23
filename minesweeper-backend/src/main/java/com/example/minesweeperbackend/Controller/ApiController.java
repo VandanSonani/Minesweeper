73247575e029e084e0ad64b775feaea06192658c
Prince <princesonani9112@gmail.com>
2024-12-23 16:44:26 -0600

    create gameboard member to hold all stuff

--- minesweeper-backend/src/main/java/com/example/minesweeperbackend/Controller/ApiController.java
+++ minesweeper-backend/src/main/java/com/example/minesweeperbackend/Controller/ApiController.java
@@ -1,6 +1,7 @@
 package com.example.minesweeperbackend.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class ApiController {
     @GetMapping("/")
     public String getPage() {
         return "Welcome to the Minesweeper API";
+    }
+
+    @GetMapping("/start")
+    public String startGame() {
+        return "Game started";
     }
 }
--- minesweeper-backend/src/main/java/com/example/minesweeperbackend/gameplay/Gameboard.java
+++ minesweeper-backend/src/main/java/com/example/minesweeperbackend/gameplay/Gameboard.java
@@ -1,15 +1,21 @@
 package com.example.minesweeperbackend.Gameplay;
 
+import java.util.Arrays;
+
 public class Gameboard {
     int bombsCount;
     int rows;
     int columns;
+    String[][] gameboard;
+
 
     public Gameboard(int rows, int columns, int bombs) {
         this.rows = rows;
         this.columns = columns;
         this.bombsCount = bombs;
+        gameboard = new String[rows][columns];
     }
+
 
     // place bomb function
     // randomly place bombbCount in row x columns
@@ -30,22 +36,47 @@ public class Gameboard {
 
             bombLocations[row][column] = true;
             System.out.println("Bomb placed at row: " + row + " column: " + column);
+            gameboard[row][column] = "B";
         }
 
-        printGameboard(bombLocations);
-    }
-
-    public void printGameboard(boolean[][] bombLocations) {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                if (bombLocations[i][j]) {
-                    System.out.print(" B ");
+                if (gameboard[i][j] == null) {
+                    gameboard[i][j] = "#";
+                }
+            }
+        }
+
+        printGameboard();
+    }
+
+//    revealCell(boolean[][] bombLocations, int row, int column) {
+//        // if bomb, game over
+//        if (bombLocations[row][column]) {
+//            System.out.println("Game Over");
+//            return;
+//        }
+//        else if (bombLocations[row][column] != true ) {
+//            System.out.println("Game Over");
+//            return;
+//        }
+//        // if number, reveal number
+//        // if empty, reveal empty
+//    }
+
+    public void printGameboard() {
+        System.out.println("Gameboard:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (gameboard[i][j].equals("#")) {
+                    System.out.print("# ");
                 } else {
-                    System.out.print(" # ");
+                    System.out.print(gameboard[i][j] + " ");
                 }
             }
             System.out.println();
         }
+        System.out.println(Arrays.deepToString(gameboard));
     }
 
 
